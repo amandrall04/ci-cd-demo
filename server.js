1bf74b9f4a23cbf371d430eecfac1bd7b76952d6
amandrall04 <singhramd1978@gmail.com>
2026-03-02 11:45:15 +0530

    fix render

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>CI/CD Version 2 🚀</h1>");
+    res.send("<h1>CI/CD Version 1 🚀</h1>");
 });
 
 app.get("/health", (req, res) => {
     res.json({ status: "running" });
 });
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// IMPORTANT LINE 👇
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
