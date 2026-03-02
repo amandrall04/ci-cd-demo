0a41b50079eef5d4b480b0b08f4257828ad87847
amandrall04 <singhramd1978@gmail.com>
2026-03-02 11:29:36 +0530

    fix: dynamic port for render

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
     res.json({ status: "running" });
 });
-
-app.listen(3000, () => console.log("Server running on port 3000: http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
