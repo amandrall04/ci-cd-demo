73cb71654a49124a986a52b0462666c625195159
amandrall04 <singhramd1978@gmail.com>
2026-03-02 11:41:12 +0530

    Version 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>CI/CD Version 1 🚀</h1>");
+    res.send("<h1>CI/CD Version 2 🚀</h1>");
 });
 
 app.get("/health", (req, res) => {
