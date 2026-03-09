0a092b55bec14fffeb6737453f3b457d3521b69d
amandrall04 <singhramd1978@gmail.com>
2026-03-09 21:32:26 +0530

    CD demo

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/health", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-// IMPORTANT LINE 👇
+
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
